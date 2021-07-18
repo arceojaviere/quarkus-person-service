dd4f2c46153ae439f5122a2267d8e29129502119
arceojaviere <arceojaviere@yahoo.com.mx>
2021-07-17 19:00:50 -0500

    Slight improvement on error handling

--- src/main/java/org/test/errorhandling/CatchAllExceptionMapper.java
+++ src/main/java/org/test/errorhandling/CatchAllExceptionMapper.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.jboss.logging.Logger;
 import org.test.errorhandling.dto.Status;
 import org.test.errorhandling.dto.StatusBean;
  
@@ -14,6 +15,8 @@ import org.test.errorhandling.dto.StatusBean;
 @Provider
 public class CatchAllExceptionMapper implements ExceptionMapper<Throwable> 
 {
+
+    private static final Logger LOGGER = Logger.getLogger(CatchAllExceptionMapper.class);
 
     private Jsonb marshaller = JsonbBuilder.create();
 
@@ -25,7 +28,10 @@ public class CatchAllExceptionMapper implements ExceptionMapper<Throwable>
         status.setDescription("Unknown Error");
         status.setExceptionType(exception.getClass().getName().toString());
         status.setMessage(exception.getMessage());
-
+        
+        if(CatchAllExceptionMapper.LOGGER.isDebugEnabled()){
+            exception.printStackTrace(System.err);
+        }
 
         return Response.status(javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR).entity(this.marshaller.toJson(status)).build();  
 
